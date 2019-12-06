61303f1e396366d0f89d4cde8fb7d96ad8489fc3
Fika <afzalov98@gmail.com>
2019-12-06 03:31:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 function dwarfRollCall(dwarves) {
+  
+    var string = []
+    for(var i = 0; i < dwarves.length; i++) {
+   string.push(((i + 1) + "." + dwarves[i]))
+    
+    }
+    console.log(string.join(" "))
 }
 
 function summonCaptainPlanet(planeteerCalls){
 }
 
+
+
 function longPlaneteerCalls(words) {
 }
+
+
 
 function findTheCheese (foods) {
 }
