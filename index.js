8bc3ca8052e948361bd199bfa9db5aa31e7adc28
Fika <afzalov98@gmail.com>
2019-12-06 03:32:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function dwarfRollCall(dwarves) {
-  
+    var string = []
+    for(var i = 0; i < arr.length; i++) {
+   string.push(((i + 1) + "." + arr[i]))
+    
+    }
+    console.log(string.join(" "))
     
 function summonCaptainPlanet(planeteerCalls){
 }
